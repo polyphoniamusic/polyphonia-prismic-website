f001fecc0c8863ef5ca9e4a558baf04a622e2ada
SoundLnk <polyphoniamusic.mgmt@gmail.com>
2023-10-30 15:16:21 +0100

    Header GSAP Bug fixed

--- app/components/Header/HeaderComponent.tsx
+++ app/components/Header/HeaderComponent.tsx
@@ -19,7 +19,7 @@ export default function Header({settings} : { settings: any}) {
 
     useEffect(() => {
         // Create your GSAP animation
-        const showAnim = gsap.from('header', {
+        const showAnim = gsap.from('.header-nav-block, .header-nav-mobile', {
             yPercent: -200,
             paused: true,
             duration: 0.5
@@ -39,28 +39,31 @@ export default function Header({settings} : { settings: any}) {
     return (
         <header id="home">
             <div className="header-container">
-                {/* Navigation Logo */}
-                <a className="header-logo-block" href="/">
-                    <PrismicNextImage field={settings.data.site_logo} className="site-logo"/>
-                </a>
 
-                {/* Navigation Menu */}
-                <nav className="header-nav">
-                    <ul className="header-nav-menu">
-                        {settings.data.navigation.map((item: { label: Key | null | undefined; }, index: Key | null | undefined) => (
-                            <li className="header-nav-button" key={item.label}>
-                                <Link data={item} index={index} key={index} />
-                            </li>
-                        ))}
-                    </ul>
-                    <ul className="header-nav-socials">
-                        {settings.data.social_navigation.map((item: { label: Key | null | undefined; }, index: Key | null | undefined) => (
-                            <li className="header-nav-link" key={item.label}>
-                                <Link data={item} index={index} key={index} />
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
+                <div className="header-nav-block">
+                    {/* Navigation Logo */}
+                    <a className="header-logo-block" href="/">
+                        <PrismicNextImage field={settings.data.site_logo} className="site-logo"/>
+                    </a>
+
+                    {/* Navigation Menu */}
+                    <nav className="header-nav">
+                        <ul className="header-nav-menu">
+                            {settings.data.navigation.map((item: { label: Key | null | undefined; }, index: Key | null | undefined) => (
+                                <li className="header-nav-button" key={item.label}>
+                                    <Link data={item} index={index} key={index} />
+                                </li>
+                            ))}
+                        </ul>
+                        <ul className="header-nav-socials">
+                            {settings.data.social_navigation.map((item: { label: Key | null | undefined; }, index: Key | null | undefined) => (
+                                <li className="header-nav-link" key={item.label}>
+                                    <Link data={item} index={index} key={index} />
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                </div>
             </div>
             <div className="header-container-mobile">
                 {/* Mobile Navigation Menu */}
@@ -68,8 +71,8 @@ export default function Header({settings} : { settings: any}) {
                         <a href="/">
                             <PrismicNextImage field={settings.data.site_logo} className="site-logo"/>
                         </a>
+                        <Burger openMenu={() => {setMenuIsOpen(true)}}/>
                     </nav>
-                    <Burger openMenu={() => {setMenuIsOpen(true)}}/>
                     <AnimatePresence mode="wait">
                         {
                             menuIsOpen && <>
