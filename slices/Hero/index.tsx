6052955644ce0313db0f98ecb6dcfb8119009b3e
SoundLnk <polyphoniamusic.mgmt@gmail.com>
2023-10-30 18:22:46 +0100

    Improvments

--- slices/Hero/index.tsx
+++ slices/Hero/index.tsx
@@ -60,17 +60,18 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
     >
       {/* Master FX Layer */}
-      <div className="master-fx"></div>
-      <div className="master-fx-overlay"></div>
       <div className="hero-container">
         <PrismicNextImage field={slice.primary.logo} className="hero-logo"/>
         <Link href="#showroll" className="hero-icon">
           <PrismicNextImage field={slice.primary.icon}/>
         </Link>
         {/*<video ref={videoRef} className="hero-video" autoPlay muted loop src="/assets/videos/polyphonia-website-hero-video.mp4"></video>*/}
+      <div className="hero-video-container">
+        <div className="master-fx"></div>
+        <div className="master-fx-overlay"></div>
         <video
           ref={videoRef}
-          className="hero-video"
+          className="hero-video-container"
           autoPlay
           muted
           playsInline
@@ -79,7 +80,12 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
           poster="/assets/images/polyphonia-website-hero-poster.jpg"
           src="/assets/videos/polyphonia-website-hero-video.mp4"
         ></video>
-       
+        </div>
+        {/*<iframe width="1920" height="1080" className="hero-video" src="https://www.youtube.com/embed/IN7Y-hX-uV4?autoplay=1&loop=1&controls=0&rel=0&showinfo=0&modestbranding=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <div className="hero-video-container">
+          <iframe className="hero-video" src="https://player.vimeo.com/video/879473105?badge=0&amp;autopause=0&amp;quality_selector=1&amp;player_id=0&amp;app_id=58479" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" title="POLYPHONIA MUSIC - WEBISTE HOME VIDEO"></iframe>
+        </div>
+        <script src="https://player.vimeo.com/api/player.js"></script>*/}
       </div>
     </section>
   );
