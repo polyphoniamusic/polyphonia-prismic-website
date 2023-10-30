524cc91e3adcb838a0e81a5dcf6858f26e08ba6e
SoundLnk <polyphoniamusic.mgmt@gmail.com>
2023-10-30 11:48:29 +0100

    Navbar component deleted

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ import HeaderComponent from '@/app/components/Header/HeaderComponent'
 // Import Priscmic Data Client
 import { createClient } from '@/prismicio';
 
-export default async function Header() {
+export default async function Header() {    
     
     const client = createClient();  
     const settings = await client.getSingle("settings");
