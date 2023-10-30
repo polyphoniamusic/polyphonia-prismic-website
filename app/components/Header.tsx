8d24d96f380ad790055f3dcaffaa81b1503dea15
SoundLnk <polyphoniamusic.mgmt@gmail.com>
2023-10-30 11:39:57 +0100

    Header param missing fixed

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,20 +1,11 @@
 //'use client';
 
-import Link from 'next/link';
-import { useState, useEffect } from 'react';
-
-import Burger from '@/app/components/Header/Burger';
-import Stairs from '@/app/components/Header/Stairs';
-import Menu from '@/app/components/Header/Menu';
-import { AnimatePresence } from 'framer-motion';
-
 import HeaderComponent from '@/app/components/Header/HeaderComponent'
 
 // Import Priscmic Data Client
 import { createClient } from '@/prismicio';
-import { PrismicNextLink, PrismicNextImage  } from '@prismicio/next';
 
-export default async function Header({}) {
+export default async function Header() {
     
     const client = createClient();  
     const settings = await client.getSingle("settings");
